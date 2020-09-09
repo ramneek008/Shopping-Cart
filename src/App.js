65c6ce31b987538d3d125963e108cefabd696cc4
ramneek008 <singhramneek274@gmail.com>
2020-09-09 19:18:12 +0530

    adding items to cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import { toast } from 'react-toastify';
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const addInCart = item => {
+    const isAlreadyAdded = cartItems.findIndex(function(array){
+      return array.id === item.id
+    })
+
+    if(isAlreadyAdded !== -1){
+      toast("Already in Cart",{
+        type:"error"
+      })
+    }
+
+    setCartItems([...cartItems, item]);
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
