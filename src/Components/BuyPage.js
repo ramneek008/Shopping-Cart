1115527edc6c44f5baee74c2f859e3c0f4320a3e
ramneek008 <singhramneek274@gmail.com>
2020-09-13 20:23:10 +0530

    fetching photos from local url

--- src/Components/BuyPage.js
+++ src/Components/BuyPage.js
@@ -7,17 +7,25 @@ const apiKey = "INSERT_YOUR_KEY_HERE";
 
 const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
 
+const localurl = "https://jsonkeeper.com/b/3YRX";
+
 const BuyPage = ({addInCart}) => {
 
     const [product, setProduct] = useState([]);
 
+    // const fetchPhotos = async () => {
+    //     const response = await Axios.get(url,{
+    //         header: {
+    //             Authorization: apiKey
+    //         }
+    //     });
+    // }
+
     const fetchPhotos = async () => {
-        const response = await Axios.get(url,{
-            header: {
-                Authorization: apiKey
-            }
-        });
+        const {data} = await Axios.get(localurl);
     }
+
+    const {photos} = data;
 
     useEffect(() =>{
         fetchPhotos()
